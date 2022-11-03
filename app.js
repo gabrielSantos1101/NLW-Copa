51849cb279f2272adf89d29c00833daf60131b64
Gabriel-Santos01 <gabriel.adv45@outlook.com>
2022-11-03 14:52:50 -0300

    update app files

--- app.js
+++ app.js
@@ -63,46 +63,14 @@ const matches = [
   }
 ]
 
-function creatCard(date, day, hour, team1, team2) {
+function creatCard(date, day, matches) {
   return `
 <div class="card">
             <div class="date">
               <h2><span>${date}</span> ${day}</h2>
             </div>
             <ul>
-              <li>
-                <img
-                  src="./files/Flags/icon-${team1}.svg"
-                  alt="Bandeira do ${team1}"
-                />
-                <p>${hour}</p>
-                <img
-                  src="./files/Flags/icon-${team2}.svg"
-                  alt="Bandeira de ${team2}"
-                />
-              </li>
-              <li>
-                <img
-                  src="./files/Flags/icon-${team1}.svg"
-                  alt="Bandeira do ${team1}"
-                />
-                <p>${hour}</p>
-                <img
-                  src="./files/Flags/icon-${team2}.svg"
-                  alt="Bandeira da ${team2}"
-                />
-              </li>
-              <li>
-                <img
-                  src="./files/Flags/icon-${team1}.svg"
-                  alt="Bandeira da ${team1}"
-                />
-                <p>${hour}</p>
-                <img
-                  src="./files/Flags/icon-${team2}.svg"
-                  alt="Bandeira do ${team2}"
-                />
-              </li>
+              ${matches}
             </ul>
           </div>
 `
@@ -110,33 +78,31 @@ function creatCard(date, day, hour, team1, team2) {
 
 function createGame(team1, hour, team2) {
   return `
-  <li>
-  <img src="./assets/icon-${team1}.svg" alt="Bandeira ${team1} ${team1}">
-  <strong>${hour}</strong>
-  <img src="./assets/icon-${team2}.svg" alt="Bandeira ${team2} ${team2}">
-</li>
-  `
+    <li>
+      <img src="./files/Flags/icon-${team1}.svg" alt="Bandeira do ${team1}">
+      <strong>${hour}</strong>
+      <img src="./files/Flags/icon-${team2}.svg" alt="Bandeira do ${team2}">
+    </li>
+`
 }
 
-document.querySelector('#app').innerHTML = `
-        <div id="cards">
-          ${creatCard('24/11', 'Domingo', '09:00', 'azerbaijan', 'zimbabwe')}
-          ${creatCard('25/11', 'Segunda', '10:30', 'brazil', 'angola')}
-          ${creatCard('26/11', 'Terça', '08:30', 'wales', 'vanuatu')}
-          ${creatCard(
-            '27/11',
-            'Quarta',
-            '05:00',
-            'uzbekistán',
-            'united states'
-          )}
-          ${creatCard(
-            '28/11',
-            'Quinta',
-            '12:30',
-            'virgin islands',
-            'vatican city'
-          )}
-          ${creatCard('29/11', 'Sexta', '11:15', 'tibet', 'togo')}
-        </div>
-`
+document.querySelector('#cards').innerHTML =
+  creatCard(
+    '24/11',
+    'Domingo',
+    createGame('azerbaijan', '09:00', 'zimbabwe') +
+      createGame('azerbaijan', '09:00', 'zimbabwe')
+  ) +
+  creatCard('25/11', 'Segunda', createGame('brazil', '10:30', 'angola')) +
+  creatCard('26/11', 'Terça', createGame('wales', '08:30', 'vanuatu')) +
+  creatCard(
+    '27/11',
+    'Quarta',
+    createGame('uzbekistán', '05:00', 'united states')
+  ) +
+  creatCard(
+    '28/11',
+    'Quinta',
+    createGame('virgin islands', '12:30', 'vatican city')
+  ) +
+  creatCard('29/11', 'Sexta', createGame('tibet', '11:15', 'togo'))
